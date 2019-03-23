ceb5116bb31482459d8157db2f2e49ce0a3efad0
Bryan <ditursibj@gmail.com>
2019-03-22 20:06:57 -0400

    Ad desolate sea app

---
+++ desolate-sea-60913/routes/index.js
@@ -0,0 +1,58 @@
+var express = require("express");
+var router = express.Router();
+var passport = require("passport");
+var User = require("../models/user");
+
+//LANDING PAGE
+router.get("/", function(req, res){
+    res.render("landing");
+});
+
+//********************************************************
+//AUTHORIZATION ROUTES
+//show register form
+router.get("/register", function(req, res){
+    res.render("register");
+});
+
+//handle sign up logic
+router.post("/register", function(req, res){
+    //create a variable to store the logic for creting a new username
+    var newUser = new User({username: req.body.username});
+    User.register(newUser, req.body.password, function (err, user){
+        if(err){
+            req.flash("error", err.message);
+            return res.redirect("/register");
+        }
+        //if sign up successful, redirect to campgrounds page
+        passport.authenticate("local")(req, res, function(){
+            req.flash("success", "Thanks for Registering " + user.username);
+            res.redirect("/campgrounds");
+        });
+    });
+});
+
+//show login form
+router.get("/login", function(req, res){
+    res.render("login");
+});
+
+//handling login logic
+router.post("/login", passport.authenticate("local",
+    {
+        successRedirect: "/campgrounds",
+        failureRedirect: "/login",
+        failureFlash: true
+    }), function(req, res){
+});
+
+// logout route
+router.get("/logout", function(req, res){
+   req.logout();
+   req.flash("success", "Logged you out!");
+   res.redirect("/campgrounds");
+});
+
+
+
+module.exports = router;
